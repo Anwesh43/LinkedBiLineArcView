4f853f2073c30c162c58131f20f7cfa81ba74e02
Anwesh43 <anweshthecool0@gmail.com>
2018-10-11 11:52:43 +0530

    adding BiLineArc class

--- bilinearcview/src/main/java/com/anwesh/uiprojects/bilinearcview/BiLineArcView.kt
+++ bilinearcview/src/main/java/com/anwesh/uiprojects/bilinearcview/BiLineArcView.kt
@@ -145,4 +145,26 @@ class BiLineArcView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiLineArc(var i : Int) {
+        private var curr : BLANode = BLANode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
