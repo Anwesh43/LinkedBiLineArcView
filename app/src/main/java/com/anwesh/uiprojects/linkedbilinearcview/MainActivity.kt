7d15b81d9e95a9584ebd8a752c4c5af692d92c44
Anwesh43 <anweshthecool0@gmail.com>
2018-10-11 12:01:33 +0530

    adding static create method

--- app/src/main/java/com/anwesh/uiprojects/linkedbilinearcview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbilinearcview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbilinearcview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.bilinearcview.BiLineArcView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiLineArcView.create(this)
     }
 }
