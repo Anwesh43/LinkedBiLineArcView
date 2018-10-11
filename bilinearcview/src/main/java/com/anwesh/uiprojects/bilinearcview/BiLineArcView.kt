24cc4fee098ce5a40eaec41c820c4245a77eaa1a
Anwesh43 <anweshthecool0@gmail.com>
2018-10-11 11:31:42 +0530

    importing libraries and defining nodes

--- bilinearcview/src/main/java/com/anwesh/uiprojects/bilinearcview/BiLineArcView.kt
+++ bilinearcview/src/main/java/com/anwesh/uiprojects/bilinearcview/BiLineArcView.kt
@@ -3,3 +3,13 @@ package com.anwesh.uiprojects.bilinearcview
 /**
  * Created by anweshmishra on 11/10/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+
+val nodes : Int = 5
